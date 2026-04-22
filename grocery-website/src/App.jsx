e0aa7d04b9d0238e1c31d2282ad812fc4a49c4d3
Rahul kumar <157276729+rahoolkr@users.noreply.github.com>
2026-04-22 18:05:32 +0530

    Navbar done

---
+++ grocery-website/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './index.css'
+import Home from './Components/Home/Home'
+
+function App() {
+  return (
+    <>
+       <Home/>
+    </>
+  )
+}
+
+export default App
