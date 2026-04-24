f19121589b88848a3c675c24637d336560aa78ee
Rahul kumar <157276729+rahoolkr@users.noreply.github.com>
2026-04-24 18:42:52 +0530

    Mobile menu done

---
+++ grocery-website/src/Components/Category/Category.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Category() {
+  return (
+     <section>
+        <div className='py-20'>
+             <div className='w-fit mx-auto '>
+                <h2 className='text-5xl font-bold'>
+                     <span className='text-orange-500'>Shop </span> by Category
+                </h2>
+                <div></div>
+             </div>
+        </div>
+     </section>
+  )
+}
--- grocery-website/src/Components/Home/Home.jsx
+++ grocery-website/src/Components/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
+import Hero from '../Hero/hero'
+import Category from '../Category/Category'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
+      <Hero/>
+      <Category/>
     </div>
   )
 }
