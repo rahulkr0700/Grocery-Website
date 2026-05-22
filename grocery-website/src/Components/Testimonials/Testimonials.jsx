ce79a085f1b28c2e1e5d8f7960b470eb385e22a9
Rahul kumar <157276729+rahoolkr@users.noreply.github.com>
2026-05-23 01:34:00 +0530

    Footer Done

---
+++ grocery-website/src/Components/Testimonials/Testimonials.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Heading from '../Heading/Heading'
+
+export default function Testimonials() {
+  return (
+    <section>
+        <div className='max-w-[1400px] px-10 mx-auto py-20'>
+            <Heading highlight="Customers" heading="Saying"/>
+        </div>
+    </section>
+  )
+}
